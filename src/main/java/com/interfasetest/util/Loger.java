60baaf7248ba9fa4e1c980028b1b761acfd2c81f
Soroke <2252501946@qq.com>
2017-03-03 23:52:23 +0800

    testPush

--- src/main/java/com/interfasetest/util/Loger.java
+++ src/main/java/com/interfasetest/util/Loger.java
@@ -1,7 +1,13 @@
 package com.interfasetest.util;
 
+import org.testng.annotations.Test;
+
 /**
  * Created by han on 2017/3/3.
  */
 public class Loger {
+    @Test
+    public void test() {
+        System.err.println("world");
+    }
 }
