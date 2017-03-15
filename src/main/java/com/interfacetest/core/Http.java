d912613592f5ef4c76e6416c1fbb97c15f7cd56d
hanminggui <909192503@qq.com>
2017-03-15 22:39:08 +0800

    提交http

--- src/main/java/com/interfacetest/core/Http.java
+++ src/main/java/com/interfacetest/core/Http.java
@@ -1,13 +1,58 @@
 package com.interfacetest.core;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.Map;
+
 /**
  * Created by han on 2017/3/3.
+ * 提供GET和POST请求发送功能、设置header功能
  */
 public class Http {
+    private String url;
+    private String param;
+    private String header;
 
-    public String doPost(){
+    public Http(){
+
+    }
+    public Http(String url){
+        this.url=url;
+    }
+
+    public Http(String url, String param) {
+    }
+
+    public Http(String url, String param, String headers) {
+    }
+
+    public Request doPost(){
 
         return null;
     }
 
+    static CloseableHttpClient closeableHttpClient = HttpClientBuilder.create().build();
+    public Request doGet(){
+        HttpGet httpGet = new HttpGet("http://www.cnblogs.com/loveyakamoz/archive/2011/07/21/2113252.html");
+        HttpResponse response = null;
+        try {
+            response = closeableHttpClient.execute(httpGet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+        return null;
+    }
+
+
+
+    public static String post(String s, Map<String, String> params) {
+        return null;
+    }
 }
