90297e69c12a35c613f2a2470476c3185ba5eb01
Soroke <2252501946@qq.com>
2017-03-29 18:34:24 +0800

    0329

--- src/main/java/com/interfacetest/core/HttpT.java
+++ src/main/java/com/interfacetest/core/HttpT.java
@@ -1,17 +1,26 @@
 package com.interfacetest.core;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
+
+import static org.apache.commons.codec.Charsets.UTF_8;
 
 /**
  * Created by song on 17/3/28.
@@ -22,8 +31,9 @@ public class HttpT {
     private String encode = "utf-8";
     private Map<Object,Object> params;
     public Logger log = Logger.getLogger(this.getClass());
-
     Request req = new Request();
+    List<NameValuePair> param = new ArrayList<NameValuePair>();
+
 
     public HttpT() {}
 
@@ -32,8 +42,10 @@ public class HttpT {
         req.setUrl(url);
     }
 
-    public String getURL() {
-        return url;
+    public HttpT(String url,Map<Object,Object> params) {
+        this.url = url;
+        req.setUrl(url);
+        this.params = params;
     }
 
     static CloseableHttpClient closeableHttpClient = HttpClientBuilder.create().build();
@@ -64,12 +76,53 @@ public class HttpT {
             }
         }
 
-
-
-        response.getEntity();
-
         return null;
     }
 
+    public void post() {
+        Long startTime,endTime;
+        log.info("------------------------开始请求------------------------");
+        log.info("接口类型：post");
+        log.info("接口URL为：" + url);
+        startTime = new Date().getTime();
+        HttpPost post = new HttpPost(url);
+        post.setEntity(addParams(params));
+
+        CloseableHttpResponse response = null;
+        try {
+            response = closeableHttpClient.execute(post);
+            endTime = new Date().getTime();
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                log.info("接口响应返回值为：" + EntityUtils.toString(entity,encode));
+            }
+            log.info("接口响应时间为：" + (endTime - startTime) + "ms");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try{
+                //关闭连接，释放资源
+                response.close();
+                log.info("------------------------请求结束------------------------");
+            } catch(IOException ioqe) {
+                log.error("关闭请求失败");
+                ioqe.printStackTrace();
+            }
+        }
+    }
+
+
+    /**
+     * create by song
+     * 循环添加参数
+     */
+    public HttpEntity addParams(Map<Object,Object> params) {
+        for (Map.Entry<Object,Object> entry : params.entrySet()) {
+            log.info("参数：\"" +entry.getKey() + "\":\"" + entry.getValue() + "\"");
+            param.add(new BasicNameValuePair(entry.getKey().toString(), entry.getValue().toString()));
+        }
+        HttpEntity param1 = new UrlEncodedFormEntity(param,UTF_8);
+        return param1;
+    }
 
 }
--- src/test/java/cases/Case_c.java
+++ src/test/java/cases/Case_c.java
@@ -1,6 +1,10 @@
 package cases;
 
+import com.interfacetest.core.HttpT;
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by han on 2017/3/3.
@@ -10,6 +14,11 @@ public class Case_c {
 
     @Test
     public void test(){
+        Map<Object,Object> param = new HashMap<Object, Object>();
+        param.put("pageSize",25);
+        param.put("areaCode","880000");
+        HttpT http = new HttpT("http://apps.faxuan.net/appbss/service/getappbasenewslist",param);
+        http.post();
 
     }
 }
