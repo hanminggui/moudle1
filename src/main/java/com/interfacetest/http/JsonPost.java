d057554b2862d6ca2eb47013f75021a0bc288efb
hanminggui <909192503@qq.com>
2017-04-01 00:37:48 +0800

    搭了个架子

--- src/main/java/com/interfacetest/http/JsonPost.java
+++ src/main/java/com/interfacetest/http/JsonPost.java
@@ -4,4 +4,15 @@ package com.interfacetest.http;
  * Created by han on 2017/3/31.
  */
 public class JsonPost extends Http {
+
+    public JsonPost(){
+        super();
+        setType(RequestType.POST);
+    }
+
+    public Request send(){
+
+        return null;
+    }
+
 }
