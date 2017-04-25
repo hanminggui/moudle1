d13968af90b28de2b097d192005b59b9f63523df
hanminggui <909192503@qq.com>
2017-04-26 01:01:12 +0800

    重写JsonHelper

--- src/main/java/com/interfacetest/util/JsonHelper.java
+++ src/main/java/com/interfacetest/util/JsonHelper.java
@@ -1,6 +1,8 @@
 package com.interfacetest.util;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
 
 /**
@@ -8,60 +10,78 @@ import com.alibaba.fastjson.JSONObject;
  */
 public class JsonHelper {
 
-    public static String getValue(String json, String keys){
-        return getValue(json, keys.split("\\."));
+    public static Object getValue(Object json, String key){
+        Object obj = getValue2(json, key);
+        if(obj instanceof notFond){
+            throw new JSONException("没有从"+json+"中找到" + key.substring(0, key.indexOf(((notFond) obj).getValie())));
+        }else {
+            return obj;
+        }
     }
 
-    /**
-     *
-     * 取出Json中指定的值
-     * 支持所有情况的格式
-     * @param json  目标json
-     * @param keys  要取出值的key的完整路径 例如：
-     *              json={"a":[{"b":1},{"c":"c"}],"d":{"e":"e"}}
-     *              keys=d.e return e
-     *              keys=a[0].b  return 1        可以写全路径key加数组下标(从0开始)
-     *              keys=a.b  return 1          如果路径中包含数组，没有写数组下标默认取第0个数组里的key
-     *              keys=a[1].c  return c
-     *              keys=a.c  return null       路径中包含数组时第0个数组里没有key=c所以返回null
-     * @return
-     */
-    public static String getValue(String json, String keys[]) {
-        if (null == keys || keys.length == 0) {
-            return null;
-        }
+    private static Object getValue2(Object json, String key) {
+        key = key.replace("]", "");
+        key = key.replace("[", ".");
+        String keys[] = key.split("\\.");
 
-        for(int i=0; i<keys.length; i++){
-            if(hasIndex(keys[i])){
-                if(isIndex(keys[i])){
-                    json = JSON.parseArray(json).getString(getIndex(keys[i]));
+        try{
+            if(json instanceof JSONArray){
+                if(keys.length==1){
+                    return ((JSONArray) json).get(Integer.parseInt(keys[0]));
                 }else {
-                    String keyName = keys[i].substring(0, keys[i].indexOf("["));
-                    int keyIndex = getIndex(keys[i]);
-                    json = JSON.parseObject(json).getJSONArray(keyName).getString(keyIndex);
-                }
-            }else {
-                try{
-                    json = JSON.parseObject(json).getString(keys[i]);
-                }catch (ClassCastException e){
-                    json = JSON.parseObject(JSON.parseArray(json).getString(0)).getString(keys[i]);
+                    return getValue2(((JSONArray) json).get(Integer.parseInt(keys[0])), key.substring(key.indexOf(".")+1));
                 }
             }
+            if(json instanceof JSONObject){
+                if(keys.length==1){
+                    return ((JSONObject) json).get(keys[0]);
+                }else {
+                    return getValue2(((JSONObject) json).get(keys[0]), key.substring(key.indexOf(".")+1));
+                }
+            }
+            if(json instanceof String){
+                return getValue2(JSON.parse((String) json), key);
+            }
+        }catch (JSONException e){
+            return new notFond(keys[0]);
         }
 
-        return json+ "";
+        return null;
     }
 
-    private static boolean hasIndex(String str){
-        return str.contains("[") && str.contains("]");
+    public Object getBByA(Object json, String key, String ak){
+        String b = key.split("=")[1];
+        key = key.split("=")[0];
+        JSONArray  ja = (JSONArray) getValue2(json, key.substring(0, key.lastIndexOf(".")));
+        for(int i=0; i< ja.size(); i++){
+            JSONObject jo = ja.getJSONObject(i);
+            if(jo.get(key.substring(key.lastIndexOf(".")+1)).equals(b)){
+                return jo.get(ak);
+            }
+        }
+        return null;
     }
 
-    private static boolean isIndex(String str){
-        return str.startsWith("[") && str.endsWith("]");
+    public static String getArrNextKey(String json, String key){
+        JSONArray ja = JSON.parseArray(json);
+        for(int i=0; i<ja.size()-1; i+=2){
+            if(ja.get(i).toString().equals(key)){
+                return ja.get(i+1).toString();
+            }
+
+        }
+        return null;
     }
 
-    private static int getIndex(String index){
-        return Integer.parseInt(index.substring(index.indexOf("[")+1, index.indexOf("]")));
+    static class notFond{
+        String valie;
+        public notFond(String valie){
+            this.valie = valie;
+        }
+        public String getValie(){
+            return this.valie;
+        }
     }
+
 
 }
--- src/test/java/cases/Case_c.java
+++ src/test/java/cases/Case_c.java
@@ -1,9 +1,8 @@
 package cases;
 
+import com.alibaba.fastjson.JSON;
+import com.interfacetest.util.JsonHelper;
 import org.testng.annotations.Test;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by han on 2017/3/3.
@@ -13,9 +12,28 @@ public class Case_c {
 
     @Test
     public void test(){
-        Map<Object,Object> param = new HashMap<Object, Object>();
-        param.put("pageSize",25);
-        param.put("areaCode","880000");
+        String json = "{\"a\":[{\"b\":{\"c\":[{},{\"d\":1}]}}]}";
+
+
+        System.out.println(JsonHelper.getValue2(json, "a[0].b.c[1].d"));
+
+        String json2 = "{\"total\":150,\"status\":1,\"rows\":[{\"id\":2777,\"coursewareType\":1,\"sort\":1,\"updateTime\":\"2017-04-25 23:46:13\",\"coursewareDiscription\":\"<p>描述<br/><\\/p><br/>\",\"courseStatus\":0,\"coursewareName\":\"name\",\"courseId\":565,\"userAccount\":\"song\",\"domainCode\":\"101000000000000\",\"courseName\":\"批量添加课程1493134527810\"},{\"id\":2776,\"coursewareType\":1,\"sort\":1,\"updateTime\":\"2017-04-25 15:41:00\",\"coursewareDiscription\":\"<p>1<\\/p>\",\"courseStatus\":1,\"coursewareName\":\"042504\",\"courseId\":543,\"userAccount\":\"song\",\"domainCode\":\"101000000000000\",\"courseName\":\"必修课程0425S\"},{\"id\":2775,\"coursewareType\":1,\"sort\":1,\"updateTime\":\"2017-04-25 14:18:07\",\"coursewareDiscription\":\"<p>1<\\/p>\",\"courseStatus\":1,\"coursewareName\":\"052503\",\"courseId\":543,\"userAccount\":\"song\",\"domainCode\":\"101000000000000\",\"courseName\":\"必修课程0425S\"},{\"id\":2774,\"coursewareType\":1,\"sort\":1,\"updateTime\":\"2017-04-25 14:17:57\",\"coursewareDiscription\":\"<p>2<\\/p>\",\"courseStatus\":1,\"coursewareName\":\"052502\",\"courseId\":543,\"userAccount\":\"song\",\"domainCode\":\"101000000000000\",\"courseName\":\"必修课程0425S\"},{\"id\":2773,\"coursewareType\":1,\"sort\":1,\"updateTime\":\"2017-04-25 14:17:44\",\"coursewareDiscription\":\"<p>1<\\/p>\",\"courseStatus\":1,\"coursewareName\":\"052501\",\"courseId\":543,\"userAccount\":\"song\",\"domainCode\":\"101000000000000\",\"courseName\":\"必修课程0425S\"},{\"id\":2768,\"coursewareType\":2,\"sort\":1,\"updateTime\":\"2017-04-25 14:16:15\",\"coursewareDiscription\":\"\",\"courseStatus\":1,\"coursewareName\":\"图片课件0424\",\"courseId\":543,\"userAccount\":\"song\",\"domainCode\":\"101000000000000\",\"courseName\":\"必修课程0425S\"},{\"id\":2767,\"coursewareType\":1,\"sort\":1,\"updateTime\":\"2017-04-25 14:13:54\",\"coursewareDiscription\":\"<p>图文课件描述<\\/p>\",\"courseStatus\":1,\"coursewareName\":\"图文课件0424\",\"courseId\":543,\"userAccount\":\"song\",\"resourceUrl\":\"http://qdtestfile.faxuan.net/study/bf11ab6b8e104729bc6026051d72641b.JPG\",\"domainCode\":\"101000000000000\",\"courseName\":\"必修课程0425S\"},{\"id\":2769,\"coursewareType\":3,\"sort\":1,\"updateTime\":\"2017-04-25 14:13:38\",\"coursewareDiscription\":\"<p>视频课件<\\/p>\",\"courseStatus\":1,\"coursewareName\":\"视频课件0424\",\"courseId\":543,\"userAccount\":\"song\",\"resourceUrl\":\"http://qdtestfile.faxuan.net/study/http://file.faxuan.net/video/lffxz_150827_256.mp4\",\"domainCode\":\"101000000000000\",\"courseName\":\"必修课程0425S\"},{\"id\":2772,\"coursewareType\":1,\"sort\":2,\"updateTime\":\"2017-04-25 10:07:04\",\"coursewareDiscription\":\"反反复复\",\"courseStatus\":1,\"coursewareName\":\"2222\",\"courseId\":539,\"userAccount\":\"yanqianling\",\"domainCode\":\"101000000000000\",\"courseName\":\"添加一个选秀\"},{\"id\":2771,\"coursewareType\":1,\"sort\":1,\"updateTime\":\"2017-04-24 18:04:38\",\"coursewareDiscription\":\"<p>11<\\/p>\",\"courseStatus\":1,\"coursewareName\":\"111\",\"courseId\":536,\"userAccount\":\"song\",\"domainCode\":\"101000000000000\",\"courseName\":\"选修课程042401\"}]}";
+
+        for(int i=0; i< JSON.parseObject(json2).getJSONArray("rows").size(); i++){
+            if(JsonHelper.getValue2(json2, "rows["+i+"].courseName").equals("批量添加课程1493134527810")){
+                int id = (int) JsonHelper.getValue2(json2, "rows["+i+"].id");
+                System.out.println(id);
+            }
+        }
+
+        int index = 0;
+        while (!JsonHelper.getValue2(json2, "rows["+(index++)+"].courseName").equals("批量添加课程1493134527810")
+                && index< JSON.parseObject(json2).getJSONArray("rows").size()){}
+        int id = (int) JsonHelper.getValue2(json2, "rows["+(index-1)+"].id");
+        System.out.println(id);
+
+        System.out.println(JsonHelper.getBByA(json2, "rows.courseName=批量添加课程1493134527810", "id"));
+
 
     }
 }
